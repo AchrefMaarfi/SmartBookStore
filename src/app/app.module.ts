1330f342487e489610099dbe7d5426c328b9e607
Achref <achref.maarfi0@gmail.com>
2021-11-04 16:02:35 +0100

    test2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ResgisterComponent } from './resgister/resgister.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ResgisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
