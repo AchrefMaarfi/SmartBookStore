085724eb2ef5aff65b82a1052dc2dc81325ba06e
Achref <achref.maarfi0@gmail.com>
2021-11-11 15:32:41 +0100

    adding navigation and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -15,12 +16,27 @@ import { NavbarComponent } from './navbar/navbar.component';
     HomeComponent,
     LoginComponent,
     SignupComponent,
-    NavbarComponent
+    NavbarComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    RouterModule.forRoot([
+      {
+        path:'',
+        component: HomeComponent
+      },
+      {
+        path:'login',
+        component: LoginComponent
+      },
+      {
+        path:'signup',
+        component: SignupComponent
+      },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
